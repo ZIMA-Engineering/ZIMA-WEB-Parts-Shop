522dd36113da2abf1bcaf0ab4d406e54dc6d1085
Jakub Skokan <jakub.skokan@havefun.cz>
2015-12-07 18:45:26 +0100

    Visualize order steps

--- zwp_shop/templatetags/zwp_shop_tags.py
+++ zwp_shop/templatetags/zwp_shop_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 
 register = template.Library()
@@ -9,6 +10,21 @@ register = template.Library()
 def zwp_cart(context):
     return context
 
+
 @register.filter
 def currency(v):
     return settings.ZWP_CURRENCY(v)
+
+
+@register.inclusion_tag('zwp_shop/order_steps.html', takes_context=True)
+def zwp_order_steps(context):
+    steps = (
+        ('shipping_payment', _('Shipping and payment method')),
+        ('billing', _('Billing and delivery')),
+        ('confirm', _('Confirmation')),
+    )
+    context['steps'] = steps
+    return {
+        'steps': steps,
+        'current_step': context['wizard']['steps'].current
+    }
